1c02b5c8549fab910633ff8fe72b207a71885b52
André Luiz Cardoso <andrecardoso@gmail.com>
2019-04-11 16:29:03 -0300

    Add log when waiting for viewer check fails

--- src/network-checks/index.js
+++ src/network-checks/index.js
@@ -1,3 +1,5 @@
+const logger = require('../logging/logger');
+
 const ONE_SECOND_MILLIS = 1000;
 const TIMEOUT_MILLIS = 60000;
 const TIMEOUT_ERROR = Error('network-check-timeout');
@@ -63,7 +65,8 @@ module.exports = {
   waitForViewer(attempt = 0, timeout = 1000) { // eslint-disable-line no-magic-numbers
     return fetch("http://viewer.risevision.com")
     .then(resp => resp.ok ? resp : Promise.reject(Error(`${resp.statusText}`)))
-    .catch(() => {
+    .catch(error => {
+      logger.error('waiting for viewer', error);
       return new Promise((resolve) => setTimeout(resolve, timeout))
       .then(() => module.exports.waitForViewer(attempt + 1, timeout * attempt));
     });
